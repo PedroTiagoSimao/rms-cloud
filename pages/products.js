8f8f480967ebbe316b509a342f71c1f21be0ea72
Pedro Simao <pedrosimao@gmail.com>
2022-12-29 17:00:53 +0000

    Update products.js

--- pages/products.js
+++ pages/products.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import ListProducts from "../components/products"
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <main className="h-full pb-16 overflow-y-auto">
+      <div className="container grid px-6 pt-6 mx-auto">
+        <ListProducts />
+      </div>
+    </main>
   )
 }
 
