232d4c6af76dca8218ab1e3dc26a1ca8460067ad
Pedro Simao <pedrosimao@gmail.com>
2022-12-29 17:00:55 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
-import Clients from "../components/clients"
-import Repairs from "../components/repairs"
+import ListClients from "../components/clients"
+import ListRepairs from "../components/repairs"
+import ListQuotes from "../components/quotes"
+import ListProducts from "../components/products"
 
 const index = () => {
   return (
     <main className="h-full pb-16 overflow-y-auto">
       <div className="container grid px-6 mx-auto">
         <h2 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Olá, $user$</h2>
-        <Clients />
-        <Repairs />
+        <ListClients />
+        <ListRepairs />
+        <ListQuotes />
+        <ListProducts />
       </div>
     </main>
   )
