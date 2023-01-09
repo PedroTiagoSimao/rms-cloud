fb53daed08e1ea5028e444667c84a742830ad2dc
Pedro Simao <pedrosimao@gmail.com>
2023-01-09 16:47:07 +0000

    testing

--- pages/dashboard/repairs.js
+++ pages/dashboard/repairs.js
@@ -1,51 +1,28 @@
 import ListRepairs from "../../components/repairs"
 import PocketBase from 'pocketbase';
 import { useEffect, useState } from "react";
+import { getRepairs } from "../../lib/repairs";
 
-const pb = new PocketBase('https://rms-cloud.pockethost.io')
-pb.autoCancellation(false)
+const Repairs = async () => {
+  const [repairItems, setRepairItems] = useState('')
 
-async function getRepairData(){
-  const res = await fetch('https://rms-cloud.pockethost.io/api/collections/repairs/records')
-  const data = await res.json()
-  return data
-}
+  const getRepairs = async () => {
+    const res = await fetch('https://rms-cloud.pockethost.io/api/collections/repairs/records')
+    const data = res.json()
+    setRepairItems(data)
+  }
 
-const Repairs = () => {
-  const [repairs, setRepairs] = useState({})
-  const data = getRepairData()
-  console.log(data.items);
-
-  /*useEffect(() => {
-    const getRepairData = async () => {
-      const res = await fetch('https://rms-cloud.pockethost.io/api/collections/repairs/records')
-      const data = await res.json()
-      setRepairs(data.items)
-    }
-    getRepairData()
-  },[] )*/
+  useEffect(() => {
+    getRepairs()
+  }, [])
 
   return (
     <main className="h-full pb-16 overflow-y-auto">
       <div className="container grid px-6 pt-6 mx-auto">
-        
+        <ListRepairs repairs={repairItems.items} />
       </div>
     </main>
   )
-}
-
-export async function getServerSideProps() {
-
-    //const companyID = companyID()
-    //const resRepairs = await fetch(`https://rms-cloud.pockethost.io/api/collections/repairs/records?filter=(company=%27${companyID}%27)`)
-    const resRepairs = await fetch(`https://rms-cloud.pockethost.io/api/collections/repairs/records`)
-    const repairs = await resRepairs.json()
-
-  return {
-    props: {
-      repairs
-    },
-  }
 }
 
 export default Repairs
