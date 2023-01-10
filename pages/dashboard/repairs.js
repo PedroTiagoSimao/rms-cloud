dc875685a40293e6df94ab2bcea58b78fb423952
Pedro Simao <pedrosimao@gmail.com>
2023-01-10 11:26:51 +0000

    Update repairs.js

--- pages/dashboard/repairs.js
+++ pages/dashboard/repairs.js
@@ -1,25 +1,27 @@
 import ListRepairs from "../../components/repairs"
-import PocketBase from 'pocketbase';
 import { useEffect, useState } from "react";
-import { getRepairs } from "../../lib/repairs";
 
-const Repairs = async () => {
-  const [repairItems, setRepairItems] = useState('')
+const Repairs = () => {
+  const [repairs, setRepairs] = useState([]);
 
   const getRepairs = async () => {
-    const res = await fetch('https://rms-cloud.pockethost.io/api/collections/repairs/records')
-    const data = res.json()
-    setRepairItems(data)
-  }
+    const res = await fetch("https://rms-cloud.pockethost.io/api/collections/repairs/records");
+    const data = await res.json();
+    setRepairs(data.items);
+  };
 
   useEffect(() => {
-    getRepairs()
-  }, [])
+    getRepairs();
+  }, []);
+
+  if (repairs.length === 0) {
+    return <p>loading</p>;
+  }
 
   return (
     <main className="h-full pb-16 overflow-y-auto">
       <div className="container grid px-6 pt-6 mx-auto">
-        <ListRepairs repairs={repairItems.items} />
+        <ListRepairs repairs={repairs} />
       </div>
     </main>
   )
