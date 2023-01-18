0598211f9eb3ae5a6c9dcfa3e05c315ad1311266
Pedro Simao <pedrosimao@gmail.com>
2023-01-18 17:01:46 +0000

    deleted nextjs api endpoints

--- pages/api/clients/index.js
+++
@@ -1,7 +0,0 @@
-import PocketBase from 'pocketbase'
-const pb = new PocketBase('https://rms-cloud.pockethost.io')
-
-export default async (req, res) => {
-    const result = await pb.collection('clients').getList(1, 20)
-    res.send(result)
-}
