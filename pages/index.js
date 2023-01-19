ed82222986967009c0e4056424ec025d170e02f6
Pedro Simao <pedrosimao@gmail.com>
2023-01-19 12:43:31 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import DefaultLayout from "../components/layouts/default"
+import { MdPassword } from 'react-icons/md'
+import { AiOutlineUser } from 'react-icons/ai'
 
 export function Index () {
 
@@ -11,27 +13,34 @@ export function Index () {
               className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400"
             >
               <input
-                className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input placeholder:text-sm placeholder:ml-10"
-                placeholder="Afonso Simão"
+                className="block w-full mt-1 text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                placeholder="user@rms-cloud.pt"
               />
               <div
-                className="absolute inset-y-0 flex items-center ml-1 pointer-events-none"
+                className="absolute inset-y-0 flex items-center ml-2 pointer-events-none"
               >
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  ></path>
-                </svg>
+                <AiOutlineUser />
               </div>
+            </div>
+          </label>
+          <label className="block mt-4 text-sm">
+            <div
+              className="relative text-gray-500 focus-within:text-purple-600"
+            >
+              <input type={'password'}
+                className="block w-full pr-20 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                placeholder="Senha"
+              />
+              <div
+                className="absolute inset-y-0 flex items-center ml-2 pointer-events-none"
+              >
+                <MdPassword />
+              </div>
+              <button
+                className="absolute inset-y-0 right-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-r-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+              >
+                Entrar
+              </button>
             </div>
           </label>
         </div>
