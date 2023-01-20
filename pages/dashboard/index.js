1185c1121f0b94edcb5e47aab3bf4951856bf927
Pedro Simao <pedrosimao@gmail.com>
2023-01-20 11:43:42 +0000

    Update index.js

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -13,13 +13,10 @@ const Dashboard = () => {
           <div className="container grid px-6 mx-auto">
             <h2 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Olá, $user$</h2>
             <div className="grid gap-6 mb-8 md:mb-16 md:grid-cols-2 xl:grid-cols-4">
-              <HomeCard text={'Total de Clientes'} number={2} icon={'clients'} bgColor={'bg-blue-100'} textColor={'text-blue-500'} table={'clients'} company={organizationID} />
-              <HomeCard text={'Reparações Abertas'} number={2} icon={'repairs'} bgColor={'bg-orange-100'} textColor={'text-orange-500'} table={'repairs'} company={organizationID} />
-              <HomeCard text={'Em Parceiros'} number={2} icon={'partners'} bgColor={'bg-green-100'} textColor={'text-green-500'} table={'partners'} company={organizationID} />
-              <HomeCard text={'Orçamentos Abertos'} number={2} icon={'quotes'} bgColor={'bg-purple-100'} textColor={'text-purple-500'} table={'quotes'} company={organizationID} />
+              <HomeCard text={'Reparações em Aberto'} number={2} icon={'repairs'} bgColor={'bg-orange-100'} textColor={'text-orange-500'} table={'repairs'} organization={organizationID()} />
             </div>
-            <ListClients organization={organizationID} pagination={true} />
-            <ListRepairs organization={organizationID} pagination={true} />
+            <ListClients organization={organizationID()} pagination={true} />
+            <ListRepairs organization={organizationID()} pagination={true} />
             <ListQuotes />
             <ListProducts />
           </div>
