0912d357bb970f204c055e9a99ebab0a10802346
Pedro Simão <pedrosimao@gmail.com>
2024-03-27 21:28:56 +0000

    Update [id].js

--- pages/dashboard/repairs/[id].js
+++ pages/dashboard/repairs/[id].js
@@ -1,38 +1,38 @@
-import React, { useState, useEffect, use } from 'react'
-import { useRouter } from 'next/router';
-import PocketBase from "pocketbase"
+import React, { useState, useEffect, use } from "react";
+import { useRouter } from "next/router";
+import PocketBase from "pocketbase";
 
-import Loading from '../../../components/loading';
-import Status from '../../../components/repairs/status';
-import Attachments from '../../../components/repairs/attachments';
-import PartnerCheckBox from '../../../components/repairs/partnercheckbox';
-import PartnerDatePicker from '../../../components/repairs/datepicker';
-import Partners from '../../../components/repairs/partners';
-import Procedures from '../../../components/procedures';
-import DashboardLayout from '../../../components/layouts/dashboard'
-import Link from 'next/link';
+import Loading from "../../../components/loading";
+import Status from "../../../components/repairs/status";
+import Attachments from "../../../components/repairs/attachments";
+import PartnerCheckBox from "../../../components/repairs/partnercheckbox";
+import PartnerDatePicker from "../../../components/repairs/datepicker";
+import Partners from "../../../components/repairs/partners";
+import Procedures from "../../../components/procedures";
+import DashboardLayout from "../../../components/layouts/dashboard";
+import Link from "next/link";
 
-const pb = new PocketBase('https://rms-cloud.pockethost.io')
-pb.autoCancellation(false)
+const pb = new PocketBase("https://rms-cloud.pockethost.io");
+pb.autoCancellation(false);
 
 const Repair = () => {
-  const router = useRouter()
-  const {id} = router.query
-  const [isLoading, setLoading] = useState(false)
-  const [repairs, setRepairs] = useState([])
-  const [client, setClient] = useState([])
+  const router = useRouter();
+  const { id } = router.query;
+  const [isLoading, setLoading] = useState(false);
+  const [repairs, setRepairs] = useState([]);
+  const [client, setClient] = useState([]);
 
   const getRepairs = async () => {
-    let url = `https://rms-cloud.pockethost.io/api/collections/repairs/records?filter=(id=%27${id}%27)`
+    let url = `https://rms-cloud.pockethost.io/api/collections/repairs/records?filter=(id=%27${id}%27)`;
 
     try {
-    const res = await fetch(url)
-    const data = await res.json()
-    setRepairs(data.items)
+      const res = await fetch(url);
+      const data = await res.json();
+      setRepairs(data.items);
     } catch (err) {
-        console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   /*
   const getClient = async ({clientID}) => {
@@ -44,154 +44,197 @@ const Repair = () => {
       console.log(err)
     }
   }*/
-  
-  useEffect(() => {
-    if(!router.isReady) return
-    getRepairs()
-  }, [router.isReady])
 
-  if(repairs.length === 0) {
+  useEffect(() => {
+    if (!router.isReady) return;
+    getRepairs();
+  }, [router.isReady]);
+
+  if (repairs.length === 0) {
     return (
-    <>
+      <>
         <h4>&nbsp;</h4>
         <Loading />
-    </>
-    )  
+      </>
+    );
   }
 
   const updateRepair = async (event, id) => {
-    event.preventDefault()
-    setLoading(true)
+    event.preventDefault();
+    setLoading(true);
     const data = {
-      "equipment": event.target.equipment.value,
-      "serial": event.target.serial.value,
-      "description": event.target.description.value,
-      "notes": event.target.notes.value,
+      equipment: event.target.equipment.value,
+      serial: event.target.serial.value,
+      description: event.target.description.value,
+      notes: event.target.notes.value,
+    };
+    const record = await pb.collection("repairs").update(id, data);
+    setLoading(false);
   };
-    const record = await pb.collection('repairs').update(id, data);
-    setLoading(false)
-  }
 
   return (
     <main className="h-full pb-16 overflow-y-auto">
-      {
-        repairs.map((repair, i) => {
-          return (
-            <div key={i} >
-              <div className="container grid px-6 pt-6 mx-auto">
-                <h4 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300 flex items-center">Reparação #{repair.number}</h4>
-                <form id={i} onSubmit={(e) => updateRepair(e, repair.id)}>
-                  <div className="px-4 py-3 mb-20 bg-white rounded-lg shadow-md dark:bg-gray-800">
-                  <div className='flex flex-col md:flex-row'>
-                      <div className='flex flex-col md:w-2/3 md:mr-4 md:mb-5'>
-                          <label className="block text-sm">
-                              <span
-                                className="text-gray-700 dark:text-gray-400"
-                                on
-                                >Cliente</span>
-                              <Link href={`/dashboard/clients/${repair.client}`}>
-                              <input
-                                  className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input cursor-pointer"
-                                  placeholder="iPhone 3GS"
-                                  name="client"
-                                  defaultValue={repair.equipment}
-                                  disabled
-                                  />
-                                </Link>
-                          </label>
-                      </div>
-                      <div className='flex flex-col md:w-1/3'>
-                        <Status repairID={repair.id} repairCurrentStatus={repair.status} />    
-                      </div>
+      {repairs.map((repair, i) => {
+        return (
+          <div key={i}>
+            <div className="container grid px-6 pt-6 mx-auto">
+              <h4 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300 flex items-center">
+                Reparação #{repair.number}
+              </h4>
+              <form id={i} onSubmit={(e) => updateRepair(e, repair.id)}>
+                <div className="px-4 py-3 mb-20 bg-white rounded-lg shadow-md dark:bg-gray-800">
+                  <div className="flex flex-col md:flex-row">
+                    <div className="flex flex-col md:w-2/3 md:mr-4 md:mb-5">
+                      <label className="block text-sm">
+                        <span className="text-gray-700 dark:text-gray-400" on>
+                          Cliente
+                        </span>
+                        <Link href={`/dashboard/clients/${repair.client}`}>
+                          <input
+                            className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input cursor-pointer"
+                            placeholder="iPhone 3GS"
+                            name="client"
+                            defaultValue={repair.client}
+                            disabled
+                          />
+                        </Link>
+                      </label>
                     </div>
-                    <div className='flex flex-col md:flex-row'>
-                      <div className='flex flex-col md:w-2/3 md:mr-4'>
-                          <label className="block text-sm">
-                              <span className="text-gray-700 dark:text-gray-400">Equipamento</span>
-                              <input
-                                  className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                                  placeholder="iPhone 3GS"
-                                  name="equipment"
-                                  defaultValue={repair.equipment}
-                                  />
-                          </label>
-                      </div>
-                      <div className='flex flex-col md:w-1/3'>
-                          <label className="block text-sm">
-                              <span className="text-gray-700 dark:text-gray-400">Nº Série/IMEI</span>
-                              <input
-                                  className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                                  placeholder="201393CD212"
-                                  name="serial"
-                                  defaultValue={repair.serial}
-                                  />
-                            </label>
-                        </div>
+                    <div className="flex flex-col md:w-1/3">
+                      <Status
+                        repairID={repair.id}
+                        repairCurrentStatus={repair.status}
+                      />
                     </div>
-                    <label className="block text-sm">
-                        <span className="text-gray-700 dark:text-gray-400">Avaria</span>
+                  </div>
+                  <div className="flex flex-col md:flex-row">
+                    <div className="flex flex-col md:w-2/3 md:mr-4">
+                      <label className="block text-sm">
+                        <span className="text-gray-700 dark:text-gray-400">
+                          Equipamento
+                        </span>
                         <input
-                            className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                            placeholder="Vidro partido/Não carrega"
-                            name="description"
-                            defaultValue={repair.description}
-                            />
-                    </label>
-                    <div className='flex flex-col md:flex-row'>
-                      <div className='flex flex-col md:w-2/3 md:mr-4'>
-                          <label className="block text-sm">
-                              <span className="text-gray-700 dark:text-gray-400">Notas</span>
-                              <textarea className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                                  placeholder="..."
-                                  name="notes" rows="5"
-                                  defaultValue={repair.notes}
-                                  >
-                              </textarea>
-                          </label>
-                      </div>
-                      <div className='flex flex-col md:w-1/3'>
-                        <span className="text-gray-700 dark:text-gray-400 text-sm">Anexos</span>
-                        <Attachments />
-                      </div>
-                    </div>
-                    <div className="mt-5 md:float-right">
-                        <input type="submit" 
-                        className={`px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150  border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple 
-                        ${isLoading ? 'bg-purple-100' : 'bg-purple-600 cursor-pointer'}`}
-                        value={'Guardar'}
+                          className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                          placeholder="iPhone 3GS"
+                          name="equipment"
+                          defaultValue={repair.equipment}
                         />
-                        <div className={`relative mt-[-28px] pl-9
-                                        ${isLoading ? '' : 'hidden'}`}>
-                            <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-slate-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                            </svg>
-                        </div>
+                      </label>
+                    </div>
+                    <div className="flex flex-col md:w-1/3">
+                      <label className="block text-sm">
+                        <span className="text-gray-700 dark:text-gray-400">
+                          Nº Série/IMEI
+                        </span>
+                        <input
+                          className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                          placeholder="201393CD212"
+                          name="serial"
+                          defaultValue={repair.serial}
+                        />
+                      </label>
                     </div>
                   </div>
-                  <div className="flex flex-row px-4 py-3 mb-8  bg-white rounded-lg shadow-md dark:bg-gray-800">
-                      <PartnerCheckBox 
-                        repairID={repair.id}
-                        currentSentToPartner = {repair.sent_to_partner} />
-                      <PartnerDatePicker 
-                        repairID={repair.id}
-                        dateSentToPartner = {`startDate: "${repair.date_sent_to_partner.split(' ')[0]}", endDate: "${repair.date_sent_to_partner.split(' ')[0]}"`} />
-                      <Partners 
-                        repairID={repair.id}
-                        organizationID={repair.organizationID}
-                        repairPartnerID={repair.partner}/>
+                  <label className="block text-sm">
+                    <span className="text-gray-700 dark:text-gray-400">
+                      Avaria
+                    </span>
+                    <input
+                      className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                      placeholder="Vidro partido/Não carrega"
+                      name="description"
+                      defaultValue={repair.description}
+                    />
+                  </label>
+                  <div className="flex flex-col md:flex-row">
+                    <div className="flex flex-col md:w-2/3 md:mr-4">
+                      <label className="block text-sm">
+                        <span className="text-gray-700 dark:text-gray-400">
+                          Notas
+                        </span>
+                        <textarea
+                          className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                          placeholder="..."
+                          name="notes"
+                          rows="5"
+                          defaultValue={repair.notes}
+                        ></textarea>
+                      </label>
+                    </div>
+                    <div className="flex flex-col md:w-1/3">
+                      <span className="text-gray-700 dark:text-gray-400 text-sm">
+                        Anexos
+                      </span>
+                      <Attachments />
+                    </div>
                   </div>
-                </form>
-                <Procedures repairID={repair.id} />
-              </div>
+                  <div className="mt-5 md:float-right">
+                    <input
+                      type="submit"
+                      className={`px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150  border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple 
+                        ${
+                          isLoading
+                            ? "bg-purple-100"
+                            : "bg-purple-600 cursor-pointer"
+                        }`}
+                      value={"Guardar"}
+                    />
+                    <div
+                      className={`relative mt-[-28px] pl-9
+                                        ${isLoading ? "" : "hidden"}`}
+                    >
+                      <svg
+                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-slate-500"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          strokeWidth="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-row px-4 py-3 mb-8  bg-white rounded-lg shadow-md dark:bg-gray-800">
+                  <PartnerCheckBox
+                    repairID={repair.id}
+                    currentSentToPartner={repair.sent_to_partner}
+                  />
+                  <PartnerDatePicker
+                    repairID={repair.id}
+                    dateSentToPartner={`startDate: "${
+                      repair.date_sent_to_partner.split(" ")[0]
+                    }", endDate: "${
+                      repair.date_sent_to_partner.split(" ")[0]
+                    }"`}
+                  />
+                  <Partners
+                    repairID={repair.id}
+                    organizationID={repair.organizationID}
+                    repairPartnerID={repair.partner}
+                  />
+                </div>
+              </form>
+              <Procedures repairID={repair.id} />
             </div>
-          )
-        })
-      }
+          </div>
+        );
+      })}
     </main>
-  )
-}
+  );
+};
 
-Repair.Layout = DashboardLayout
+Repair.Layout = DashboardLayout;
 
-export default Repair
+export default Repair;
